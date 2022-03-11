ab6b7f6cb8324e884597265bd924713ea6c1f279
sbarrios93 <sbarrios93@gmail.com>
2022-03-11 13:52:47 -0300

    eda

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
-from src.parse_data import parse_combined_data, parse_qualifying_data, generate_prediction_data, _assert_and_make_dirs
+from src.parse_data import parse_combined_data, parse_qualifying_data, generate_prediction_data
+
+
+if __name__ == "__main__":
+
+    # first we need to load the data
+    parse_combined_data(force=False)
+    parse_qualifying_data(force=False)
+    # generate_prediction_data(force=True)
